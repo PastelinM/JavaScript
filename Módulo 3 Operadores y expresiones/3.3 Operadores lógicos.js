2d735af248ff76657c1166b15e36bd7396abbc1c
Mauricio Pastelin <mpastelin3@gmail.com>
2023-11-20 09:10:08 -0600

    feat: :white_check_mark: second commit

---
+++ Módulo 3 Operadores y expresiones/3.3 Operadores lógicos.js
@@ -0,0 +1,35 @@
+/**
+ * @operadoresLogicos
+ * Los operadores lógicos en js se utilizan para realizar operaciones de
+ * comparación o evaluación lógica entre valores.
+ * 
+ * @and
+ * Este operador devuelve true si ambos operadores son verdaderos.
+ */
+
+if (x > 5 && y < 10) {
+    // Se ejecuta si x es mayor que 5 y y es menor que 10
+}
+
+
+/**
+ * @or
+ * Devuelve true si al menos uno de los operadores es verdadero.
+ * 
+ */
+if (x === 5 || y === 10) {
+    // Se ejecuta si x es igual a 5 o y es igual a 10
+}
+
+/**
+ * @not 
+ * Es un operador unario que invierte el valor de su operando.
+ * 
+ */
+
+if (!x === 5) {
+    // Se ejecuta si x NO es igual a 5
+}
+
+
+
---
+++ Módulo 5 Funciones/5.2 Parametros y argumentos.js
@@ -0,0 +1,33 @@
+/**
+ * @parametros
+ * Son variables mencionadas en la definición de una función. Son nombres que se utilizan dentro de la función y actuan como contenedores para los valores que se pasan cuando la función es llamada.
+ * 
+ */
+
+/**
+ * @ejemplo
+ * Nota: a y b son parámetros de la función suma.
+ */
+
+function suma(a, b) {
+    return a + b;
+}
+
+
+/**
+ * @argumentos
+ * Son los valores reales pasados a la función cuando esta es invocada. Son valores que se asignan a los 
+ * parámetros de la función cuando se llama a esa función.
+ */
+
+
+/**
+ * @ejemplo
+ * Nota: En esta llamada a la función suma, 3 y 5 son los argumentos que se asignarán a a y b 
+ * respectivamente dentro de la función.
+ */
+
+suma(3, 5);
+
+
+
